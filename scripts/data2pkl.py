c555a13ba4fa3af087d75b890ff369a47ebea550
Tianxiao Zhao <tzh@kth.se>
2017-05-16 12:28:15 +0200

    create .pkl files

--- scripts/data2pkl.py
+++ scripts/data2pkl.py
@@ -13,7 +13,7 @@ import numpy as np
 import os
 
 
-def dir_to_dataset(glob_files, loc_train_labels = ""):
+def dir_to_dataset(glob_files, new_width = 200, new_height = 200):
     print("To be processed:\n\t %s"%glob_files)
     dataset = []
     delete_file = []
@@ -21,8 +21,9 @@ def dir_to_dataset(glob_files, loc_train_labels = ""):
     for file_count, file_name in enumerate(sorted(glob(glob_files), key = len)):
         print("\t %s files processed"%(file_count + 1))
         
-        # return a sequence object (flattened) with pixel values
+        # resize images and return a sequence object (flattened) with pixel values
         img = Image.open(file_name)
+        img = img.resize((new_width, new_height), Image.ANTIALIAS)
         pixels = list(img.getdata())
         
         # avoid potential binary images
@@ -37,25 +38,29 @@ def dir_to_dataset(glob_files, loc_train_labels = ""):
 
 
 # define read directory of a dataset
-tr_file_x_dir1 = "/Users/MMGF2/Desktop/dl_pro/datasets/MIT_Scene_Parsing/ADEChallengeData2016/images/bedroom_tr1//*.jpg"
-tr_file_x_dir2 = "/Users/MMGF2/Desktop/dl_pro/datasets/MIT_Scene_Parsing/ADEChallengeData2016/images/bedroom_tr2//*.jpg"
-tr_file_y_dir1 = "/Users/MMGF2/Desktop/dl_pro/datasets/MIT_Scene_Parsing/ADEChallengeData2016/annotations/bedroom_tr_label//*.png"
-tr_file_y_dir2 = "/Users/MMGF2/Desktop/dl_pro/datasets/MIT_Scene_Parsing/ADEChallengeData2016/annotations/bedroom_tr_label2//*.png"
+file_dir = '/Users/MMGF2/Desktop/dl_pro/datasets/MIT_Scene_Parsing/ADEChallengeData2016/'
+filename_x_1 = 'images/bedroom_tr1//*.jpg'
+filename_x_2 = 'images/bedroom_tr2//*.jpg'
+filename_y_1 = 'annotations/bedroom_tr_label1//*.png'
+filename_y_2 = 'annotations/bedroom_tr_label2//*.png'
+tr_file_r_dir = os.path.join(file_dir, filename_x_2)
 
 # read in image data
-Data, delete_file = dir_to_dataset(tr_file_y_dir1, "")
+Data, delete_file = dir_to_dataset(tr_file_r_dir)
 
 # regroup data
 train_set_x = Data
 
 # define write directory of .pkl files
-tr_x_dir1 = '/Users/MMGF2/Desktop/dl_pro/datasets/tr_x_batch_1.pkl'
-tr_x_dir2 = '/Users/MMGF2/Desktop/dl_pro/datasets/tr_x_batch_2.pkl'
-tr_y_dir1 = '/Users/MMGF2/Desktop/dl_pro/datasets/tr_y_batch_1.pkl'
-tr_y_dir2 = '/Users/MMGF2/Desktop/dl_pro/datasets/tr_y_batch_2.pkl'
+file_dir = '/Users/MMGF2/Desktop/dl_pro/datasets/'
+filename_x_1 = 'tr_x_batch_1.pkl'
+filename_x_2 = 'tr_x_batch_2.pkl'
+filename_y_1 = 'tr_y_batch_1.pkl'
+filename_y_2 = 'tr_y_batch_2.pkl'
+tr_file_w_dir = os.path.join(file_dir, filename_x_2)
 
 # write data into .pkl files
-f = open(tr_y_dir1, 'wb')
+f = open(tr_file_w_dir, 'wb')
 pickle.dump(train_set_x, f)
 f.close()
 
